7dbd21d88654b89dfeb6dcbcd38163b87baddf23
cliang868 <cliang868@gmail.com>
2021-11-15 12:11:15 -0500

    fix icon link

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,7 +9,7 @@ const Header: React.FC = () => {
   return (
     <div className="flex flex-col md:flex-row justify-between space-x-0 md:space-x-5 space-y-5 md:space-x-0">
       <div className="text-black space-x-5 text-lg flex items-center">
-        <Link href="/">
+        <Link href="https://www.solanalongnecks.com/">
           <img className="header-img" src={`/giraffe3.png`} />
         </Link>
       </div>
